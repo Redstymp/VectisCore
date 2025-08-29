356025223c5d9b7d3d45867906c1af234e93608a
Redstymp <no-email>
2025-08-30 00:50:16 +0200

    Initial commit

---
+++ Media.lua
@@ -0,0 +1,22 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "vectis_im_in_danger", [[Interface\Addons\VectisCore\sound\vectis_im_in_danger.mp3]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
---
+++ generateLuas.bat
@@ -0,0 +1,61 @@
+@echo off
+echo This script will now prepare the files for using Vectis Core
+
+echo Creating the file...
+echo local LSM = LibStub("LibSharedMedia-3.0") > Media.lua
+
+echo    BACKGROUND
+echo.>> Media.lua
+echo -- ----- >> Media.lua
+echo -- BACKGROUND >> Media.lua
+echo -- ----- >> Media.lua
+for %%F in (background\*.*) do (
+echo       %%~nF
+echo LSM:Register("background", "%%~nF", [[Interface\Addons\VectisCore\background\%%~nxF]]^) >> Media.lua
+)
+
+echo    BORDER
+echo.>> Media.lua
+echo -- ----- >> Media.lua
+echo --  BORDER >> Media.lua
+echo -- ---- >> Media.lua
+for %%F in (border\*.*) do (
+echo       %%~nF
+echo LSM:Register("border", "%%~nF", [[Interface\Addons\VectisCore\border\%%~nxF]]^) >> Media.lua
+)
+
+echo    FONT
+echo.>> Media.lua
+echo -- ----->> Media.lua
+echo --   FONT>> Media.lua
+echo -- ----->> Media.lua
+for %%F in (font\*.ttf) do (
+echo       %%~nF
+echo LSM:Register("font", "%%~nF", [[Interface\Addons\VectisCore\font\%%~nxF]]^) >> Media.lua
+)
+
+echo    SOUND
+echo.>> Media.lua
+echo -- ----->> Media.lua
+echo --   SOUND>> Media.lua
+echo -- ----->> Media.lua
+for %%F in (sound\*.*) do (
+echo       %%~nF
+echo LSM:Register("sound", "%%~nF", [[Interface\Addons\VectisCore\sound\%%~nxF]]^) >> Media.lua
+)
+
+echo    STATUSBAR
+echo.>> Media.lua
+echo -- ----->> Media.lua
+echo --   STATUSBAR>> Media.lua
+echo -- ----->> Media.lua
+for %%F in (statusbar\*.*) do (
+echo       %%~nF
+echo LSM:Register("statusbar", "%%~nF", [[Interface\Addons\VectisCore\statusbar\%%~nxF]]^) >> Media.lua
+)
+
+pause
+
+
+
+
