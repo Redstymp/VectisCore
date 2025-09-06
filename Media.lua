22396098e2827499e65c064d642359f7c8974450
Redstymp <no-email>
2025-09-06 21:18:59 +0200

    Added new sound for bonusroll weakaura

--- Media.lua
+++ Media.lua
@@ -17,11 +17,14 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 LSM:Register("sound", "vectis blamage", [[Interface\Addons\VectisCore\sound\blamage.mp3]]) 
 LSM:Register("sound", "vectis im in danger", [[Interface\Addons\VectisCore\sound\im in danger.mp3]]) 
+LSM:Register("sound", "vectis laugh canadian", [[Interface\Addons\VectisCore\sound\laugh canadian.mp3]]) 
 LSM:Register("sound", "vectis laugh", [[Interface\Addons\VectisCore\sound\laugh.mp3]]) 
 LSM:Register("sound", "vectis Why are you running_", [[Interface\Addons\VectisCore\sound\Why are you running_.mp3]]) 
+LSM:Register("sound", "vectis woooo", [[Interface\Addons\VectisCore\sound\woooo.mp3]]) 
 LSM:Register("sound", "vectis zoidberg", [[Interface\Addons\VectisCore\sound\zoidberg.mp3]]) 
 
 -- -----
 --   STATUSBAR
 -- -----
 LSM:Register("statusbar", "cobblestone", [[Interface\Addons\VectisCore\statusbar\cobblestone.blp]]) 
+LSM:Register("statusbar", "Laughing", [[Interface\Addons\VectisCore\statusbar\Laughing.blp]]) 
